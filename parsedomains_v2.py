d3da57c1a1fbb1b195a9e15379ced7782a37bd4a
Nathan Ang <nathanan@Nathans-MacBook-Pro.local>
2020-05-28 23:28:47 -0700

    speedup

--- parsedomains_v2.py
+++ parsedomains_v2.py
@@ -6,11 +6,11 @@ file_out.write("var blocked_sites_v2 = [\n")
 
 first = True
 while(line):
-    if("# [" in line):
+    if("127.0.0.1 " in line):
         if not first:
              file_out.write(",\n")
         first = False
-        line = (line.lstrip("# [")).rstrip("]\n")
+        line = (line.lstrip("127.0.0.1 ")).rstrip("]\n")
         parsed_line = "*://*." + line + "/*"
         file_out.write("\"" + parsed_line + "\"")
     line = file_in.readline()
